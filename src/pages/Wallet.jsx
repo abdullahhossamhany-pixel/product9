cb90db593e6fba3d96af897cbf3f13fde0f261f8
abdullahhossamhany-pixel <abdullahhossamhany@gmail.com>
2026-09-08 14:19:53 +0300

    Update Wallet.jsx

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 
 import { useQuery } from "@tanstack/react-query";
 import { Wallet as WalletIcon, Gift, Loader2 } from "lucide-react";
-import WalletTransactions from "@/components/wallet/WalletTransactions";
+//import WalletTransactions from "@/components/wallet/WalletTransactions";
 
 export default function Wallet() {
   const [user, setUser] = useState(null);
