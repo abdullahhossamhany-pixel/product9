468465e5735fd3f07ddd817d37f2434b1c293f2c
abdullahhossamhany-pixel <abdullahhossamhany@gmail.com>
2026-09-08 14:03:23 +0000

    Import base44Client in main entry point

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -20,6 +20,13 @@ if (typeof sdkClient.auth.redirectToLogin !== 'function') {
   };
 }
 
+// Attach to global window scope so components looking for global instances don't hang
+if (typeof window !== 'undefined') {
+  window.base44 = sdkClient;
+  window.__B44_DB__ = sdkClient;
+  globalThis.__B44_DB__ = sdkClient;
+}
+
 export const db = sdkClient;
-export const base44 = db;
-export default db;
+export const base44 = sdkClient;
+export default sdkClient;
